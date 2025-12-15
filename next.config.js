3aa8126d3729bd0a71000a1db582f02d7dfc25b9
Wafi Bin Hassan <73391954+Wafi618@users.noreply.github.com>
2025-12-15 01:40:22 -0500

    Fix: Add starxessories.cc to allowedDevOrigins in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    allowedDevOrigins: ['https://starxessories.cc'],
     reactStrictMode: true,
     images: {
         remotePatterns: [
